daebe0ced5fbe6ce189bcc118f7c01183141dac6
Raja Mahanty <rajamahanty2@gmail.com>
2025-12-01 22:43:40 +0530

    feat: integrate Tailwind CSS and add Stream Chat functionality
    - Added Tailwind CSS as a dependency and configured it in the project.
    - Created a new hook `useStreamChat` to manage Stream Chat client connection.
    - Updated `App` component to manage authentication state and routing.
    - Added API function to fetch Stream Chat token.
    - Refactored CSS to use Tailwind for styling.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,45 @@
-import { SignedIn, SignedOut } from "@clerk/clerk-react";
-import { Navigate, Route, Routes } from "react-router";
-import HomePage from "./pages/HomePage";
-import AuthPage from "./pages/AuthPage";
+import { useAuth } from "@clerk/clerk-react";
 import { withSentryReactRouterV7Routing } from "@sentry/react";
+import { Navigate, Route, Routes } from "react-router";
+import AuthPage from "./pages/AuthPage";
+import CallPage from "./pages/CallPage";
+import HomePage from "./pages/HomePage";
 
 const SentryRoutes = withSentryReactRouterV7Routing(Routes);
 
 const App = () => {
-	return (
-		<>
-			<SignedIn>
-				<SentryRoutes>
-					<Route path="/" element={<HomePage />} />
-					<Route path="/auth" element={<Navigate to="/" replace />} />
-				</SentryRoutes>
-			</SignedIn>
+	const { isSignedIn, isLoaded } = useAuth();
 
-			<SignedOut>
-				<SentryRoutes>
-					<Route path="/auth" element={<AuthPage />} />
-					<Route path="*" element={<Navigate to="/auth" replace />} />
-				</SentryRoutes>
-			</SignedOut>
-		</>
+	if (!isLoaded) null;
+
+	return (
+		<SentryRoutes>
+			<Route
+				path="/"
+				element={isSignedIn ? <HomePage /> : <Navigate to="/auth" replace />}
+			/>
+
+			<Route
+				path="/auth"
+				element={!isSignedIn ? <AuthPage /> : <Navigate to="/" replace />}
+			/>
+
+			<Route
+				path="/call/:id"
+				element={isSignedIn ? <CallPage /> : <Navigate to="/auth" replace />}
+			/>
+
+			<Route
+				path="*"
+				element={
+					isSignedIn ? (
+						<Navigate to="/" replace />
+					) : (
+						<Navigate to="/auth" replace />
+					)
+				}
+			/>
+		</SentryRoutes>
 	);
 };
 
